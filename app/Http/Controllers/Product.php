84dc925ffd2a905b0c6bb102d3c632c9ae752bd3
arjansunar <arjan.gahatrajsunar@gmail.com>
2021-08-02 14:16:23 +0545

    passing values to views using controllers

--- app/Http/Controllers/Product.php
+++ app/Http/Controllers/Product.php
@@ -17,6 +17,11 @@ class Product extends Controller
                 "name"=>"Classic Watch2", 
                 "price"=>"23", 
                 "image"=> "https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+            ],
+            [
+                "name"=>"Classic Watch3", 
+                "price"=>"23", 
+                "image"=> "https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
             ]
         ];
         return view('products')->with("products", $products);
