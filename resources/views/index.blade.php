a996e96009859d61222bee0508b5cc7899378cd3
arjansunar <arjan.gahatrajsunar@gmail.com>
2021-09-10 14:46:14 +0545

    rendering thml instead of plain text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
             <div class="bg-gray-900 bg-opacity-70 flex items-center h-full">
                 <div class="px-10 max-w-xl">
                     <h2 class="text-2xl text-white font-semibold">{{$item["name"]}}</h2>
-                    <p class="mt-2 text-gray-400">{{$item["description"]}}</p>
+                    <div class="mt-2 text-gray-400">{!! $item["description"] !!}</div>
                     <a href="/product/{{$item["id"]}}" class="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none">
                         <span>Shop Now</span>
                         <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
